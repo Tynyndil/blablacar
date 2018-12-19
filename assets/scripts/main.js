c0ad95a849315da3ec24fc4c19d869ad630e73ee
Tynyndil <thomasraineau3@gmail.com>
2018-12-20 00:47:59 +0100

    Integration deux cas

---
+++ assets/scripts/main.js
@@ -0,0 +1,33 @@
+document.querySelector("#trajectButton").addEventListener("click", loadTrajectCase)
+
+function loadTrajectCase(){
+    var mainContent = document.querySelector("#main");
+    var trajectContent = document.querySelector("#trajectCase");
+    mainContent.innerHTML = trajectContent.innerHTML;
+}
+
+document.querySelector("#walletButton").addEventListener("click", loadWalletCase);
+
+function loadWalletCase(){
+    var mainContent = document.querySelector("#main");
+    var trajectContent = document.querySelector("#walletCase");
+    mainContent.innerHTML = trajectContent.innerHTML;
+
+    document.querySelector("#lessPriceButton").addEventListener("click", reduceTicketPrice);
+    document.querySelector("#morePriceButton").addEventListener("click", increaseTicketPrice);
+}
+
+function reduceTicketPrice(){
+    var currentPrice = parseInt(document.querySelector("#ticketPrice").innerHTML);
+    currentPrice -= 1;
+    document.querySelector("#ticketPrice").innerHTML = currentPrice;
+
+}
+
+function increaseTicketPrice(){
+    var currentPrice = parseInt(document.querySelector("#ticketPrice").innerHTML);
+    currentPrice += 1;
+    document.querySelector("#ticketPrice").innerHTML = currentPrice;
+}
+
+
--- index.php
+++ index.php
@@ -7,12 +7,14 @@
             <div class="col-12 green t2000 bold">BlaBlaCar+</div>
         </div>
         <div class="clear30"></div>
-        <div class="row">
+
+        <!-- Main Page with explanations -->
+        <div class="row" id="main">
             <div class="col-2">&nbsp</div>
 
             <div class="col-8">
                 <span class="blue_sky bold">BlaBlaCar+</span> est né de votre volonté à parfaire vos trajets, vos covoiturages, et votre expérience. 
-                Car votre satisfaction est notre fer de lancec
+                Car votre satisfaction est notre fer de lance.<br/><br/>
 
                 Que vous fassiez un même trajet tous les jours, ou que vous vagabondiez à travers le territoire quand 
                 bon vous semble, vous pouvez désormais <span class="blue_sky bold">réservez 
@@ -36,19 +38,21 @@
                 <div class="row center">
                     <div class="col-6" style="border-right:2px solid #00AFF5;">
                         <span class="blue_sky bold t1500">1.</span> En choisissant un trajet<br/>
-                        <button class="blue_button">Essayez</button>
+                        <img id="road_img" src="assets/img/route.png" alt="Trajet"><br/>
+                        <button id="trajectButton" class="blue_button">Essayez</button>
                     </div>
 
                     <div class="col-6"><span class="blue_sky bold t1500">
                         2.</span> ...ou un tarif maximal<br/>
-                        <button class="blue_button">Essayez</button>
+                        <img id="wallet_img" src="assets/img/wallet.png" alt="Trajet"><br/>
+                        <button id="walletButton" class="blue_button">Essayez</button>
                     </div>
                 </div>
 
                 <div class="clear30"></div>
 
                 En optant pour la première solution, soit un choisissant <span class="blue_sky bold">un trajet prédéfini</span>
-                , vous pourrez <span class="blue_sky bold">achetez plusieurs tickets pour ce trajet ou un trajet équivalent
+                , vous pourrez <span class="blue_sky bold">achetez plusieurs tickets pour ce trajet ou un trajet du même prix
                 en une fois !</span><br/><br/>
 
                 Les avantages ? Vous n'aurez plus à payez les frais de réservation, vous pourrez planifiez vos trajets à 
@@ -65,6 +69,104 @@
             </div>
 
             <div class="col-2">&nbsp</div>
+        </div>
+
+        <!-- Page where user want to buy tickets with a traject -->
+        <div class="row" id="trajectCase" style="display:none;">
+            <div class="col-3">&nbsp</div>
+
+            <div class="col-6">
+                <div class="center blue_sky t2000 bold">
+                    Choisissez votre trajet<br/>
+                    <img id="road_img" src="assets/img/route.png" alt="Trajet"><br/>
+                </div>
+                
+                <input type="text" placeholder="Départ">
+                <input type="text" placeholder="Destination" style="margin-top:10px;">
+
+                <div class="grey_line"></div>
+
+                <span id="price_sentence">Prix moyen d'un trajet ?/?<span><br/>
+                <span id="price">? €</span>
+
+                <div class="grey_line"></div>
+
+                Nombre de tickets désirés
+
+                <div class="row">
+                    <button class="ticket_number">5</button>
+                    <button class="ticket_number">10</button>
+                    <button class="ticket_number">20</button>
+                </div>
+
+                <div class="grey_line"></div>
+
+                <div class="row">
+                    <div class="col-3">
+                        Récapitulatif<br/>
+                        <span class="blue_sky" style="border-bottom: 1px solid #EDEDED;">? x ?</span><br/>
+                        <span class="blue_sky bold">?</span>
+                    </div>
+                    <div class="col-3">&nbsp</div>
+                    <div class="col-6">Un seul de ces tickets permet d'effectuer n'importe quel trajet, à condition 
+                        que son prix soit de ? € maximum
+                    </div>
+                </div>
+
+                <div class="grey_line"></div>
+
+                <div class="center"><button class="blue_button">Acheter mes tickets</button></div>
+            </div>
+
+            <div class="col-3">&nbsp</div>
+        </div>
+
+        <!-- Page where user want to buy tickets with a price -->
+        <div class="row" id="walletCase" style="display:none;">
+            <div class="col-3">&nbsp</div>
+
+            <div class="col-6">
+                <div class="center blue_sky t2000 bold">
+                    Choisissez votre budget<br/>
+                    <img id="road_img" src="assets/img/wallet.png" alt="Trajet"><br/>
+                </div>
+
+                <div class="row">
+                    <div class="col-4 t2000 blue_sky"><button id="lessPriceButton" class="right"></button></div>
+                    <div class="col-4 center blue_sky t2000 bold"><span id="ticketPrice">00</span>,00€</div>
+                    <div class="col-4 t2000 blue_sky"><button id="morePriceButton" class="left"></button></div>
+                </div>
+
+                <div class="grey_line"></div>
+
+                Nombre de tickets désirés
+
+                <div class="row">
+                    <button class="ticket_number">5</button>
+                    <button class="ticket_number">10</button>
+                    <button class="ticket_number">20</button>
+                </div>
+
+                <div class="grey_line"></div>
+
+                <div class="row">
+                    <div class="col-3">
+                        Récapitulatif<br/>
+                        <span class="blue_sky" style="border-bottom: 1px solid #EDEDED;">? x ?</span><br/>
+                        <span class="blue_sky bold">?</span>
+                    </div>
+                    <div class="col-3">&nbsp</div>
+                    <div class="col-6">Un seul de ces tickets permet d'effectuer n'importe quel trajet, à condition 
+                        que son prix soit de ? € maximum
+                    </div>
+                </div>
+
+                <div class="grey_line"></div>
+
+                <div class="center"><button class="blue_button">Acheter mes tickets</button></div>
+            </div>
+
+            <div class="col-3">&nbsp</div>
         </div>
 
         <div class="clear50"></div>
