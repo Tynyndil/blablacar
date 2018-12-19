ebfda43c2d5b7d5353935df36eb26507a9991bf0
Tynyndil <thomasraineau3@gmail.com>
2018-12-19 19:34:59 +0100

    Integration

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>BlaBlaCar</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="https://cdn.blablacar.com/comuto3/blablacar-favicon-16x16.png">
     <link rel="stylesheet" type="text/css" media="screen" href="assets/styles/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="assets/styles/main.css" />
 </head>
@@ -15,7 +16,7 @@
         <header id="header">
             <div class="container">
                 <div class="row">
-                <div class="col-4" id="head_image">
+                <div class="col-3" id="head_image">
                     <img src="assets/img/blablacar-logo.png" alt="BlaBlacar">
                 </div>
                 <div class="col-3 blue_sky">
@@ -24,8 +25,11 @@
                 <div class="col-2 blue_sky">
                     <a href="#">Rechercher</a>
                 </div>
-                <div class="col-3 blue_sky">
+                <div class="col-2 blue_sky">
                     <a href="#">Proposer un trajet</a>
+                </div>
+                <div class="col-2 blue_sky">
+                    <a href="#">BlaBlaCar+</a>
                 </div>
                 </div>    
             </div>
--- index.php
+++ index.php
@@ -1,10 +1,74 @@
 <?php include_once("header.php");
 ?>
 
+    <div class="clear50"></div>
     <div class="container">
-        <div class="row">
-            Coucou
+        <div class="row center">
+            <div class="col-12 green t2000 bold">BlaBlaCar+</div>
         </div>
+        <div class="clear30"></div>
+        <div class="row">
+            <div class="col-2">&nbsp</div>
+
+            <div class="col-8">
+                <span class="blue_sky bold">BlaBlaCar+</span> est né de votre volonté à parfaire vos trajets, vos covoiturages, et votre expérience. 
+                Car votre satisfaction est notre fer de lancec
+
+                Que vous fassiez un même trajet tous les jours, ou que vous vagabondiez à travers le territoire quand 
+                bon vous semble, vous pouvez désormais <span class="blue_sky bold">réservez 
+                en une fois 5, 10 ou 20 trajets.</span><br/><br/>
+
+                Ne perdez plus de temps à chercher, en plus vous paierez moins cher !<br/><br/>
+
+                <span class="blue_sky bold">Rapide, pratique, économique.</span>
+
+                <div class="clear30"></div>
+
+                <div class="row center">
+                    <div class="col-12 green t2000 bold">Réservez vos trajets</div>
+                </div>
+                <div class="clear30"></div>
+                <div class="row center">
+                    <div class="col-12 blue_sky">Deux façons de profiter de BlaBlaCar+</div>
+                </div>
+                <div class="clear30"></div>
+
+                <div class="row center">
+                    <div class="col-6" style="border-right:2px solid #00AFF5;">
+                        <span class="blue_sky bold t1500">1.</span> En choisissant un trajet<br/>
+                        <button class="blue_button">Essayez</button>
+                    </div>
+
+                    <div class="col-6"><span class="blue_sky bold t1500">
+                        2.</span> ...ou un tarif maximal<br/>
+                        <button class="blue_button">Essayez</button>
+                    </div>
+                </div>
+
+                <div class="clear30"></div>
+
+                En optant pour la première solution, soit un choisissant <span class="blue_sky bold">un trajet prédéfini</span>
+                , vous pourrez <span class="blue_sky bold">achetez plusieurs tickets pour ce trajet ou un trajet équivalent
+                en une fois !</span><br/><br/>
+
+                Les avantages ? Vous n'aurez plus à payez les frais de réservation, vous pourrez planifiez vos trajets à 
+                l'avance, et même vous faire démarcher par les conducteurs eux-mêmes pour effectuer vos trajets ! <br/><br/>
+
+                Concernant la deuxième solution, c'est-à-dire en fixant <span class="blue_sky bold">un tarif maximal</span>
+                , vous pourez acheter des tickets vous permettant de <span class="blue_sky bold">faire n'importe quel 
+                trajet</span>, à condition que son prix ne dépasse pas le tarif que vous avez renseigné.<br/><br/>
+
+                De la même manière, les frais de réservation disparaissent, les trajets sont planifiables et les conducteurs
+                viennent vous démarcher !<br/><br/>
+
+                <span class="blue_sky bold t1250">Alors, vous partez quand ?</span>
+            </div>
+
+            <div class="col-2">&nbsp</div>
+        </div>
+
+        <div class="clear50"></div>
+
     </div>
 
 <?php include_once("footer.php");
